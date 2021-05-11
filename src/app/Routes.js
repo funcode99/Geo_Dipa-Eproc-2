95c7e340e29c573dd1292cbf4001d1e88a4a9352
grhgn <rahagigalih@gmail.com>
2021-05-11 09:54:55 +0700

    tambah halaman buat akun

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -5,13 +5,17 @@
  * components (e.g: `src/app/modules/Auth/pages/AuthPage`, `src/app/BasePage`).
  */
 
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Redirect, Switch, Route } from "react-router-dom";
 import { shallowEqual, useSelector } from "react-redux";
 import { Layout } from "../_metronic/layout";
 import BasePage from "./BasePage";
 import { Logout, AuthPage } from "./modules/Auth";
 import ErrorsPage from "./modules/ErrorsExamples/ErrorsPage";
+
+const UserNewAccountHomePage = lazy(() =>
+  import("./modules/UserCreate/CreateNewAccountHome")
+);
 
 export function Routes() {
   const { isAuthorized } = useSelector(
@@ -23,6 +27,7 @@ export function Routes() {
 
   return (
     <Switch>
+      <Route path="/create-account" component={UserNewAccountHomePage} />
       {!isAuthorized ? (
         /*Render auth page when user at `/auth` and not authorized.*/
         <Route>
