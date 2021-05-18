244755ac9742560b50dcd952d90a3a3cffa876c0
nafifurqon <muhammadnafi98@gmail.com>
2021-05-18 15:41:18 +0700

    wip: styling summary get data items from api

--- src/app/modules/DeliveryMonitoring/_redux/deliveryMonitoringSlice.js
+++ src/app/modules/DeliveryMonitoring/_redux/deliveryMonitoringSlice.js
@@ -1,36 +1,43 @@
-import { persistReducer } from "redux-persist";
-import { PERSIST_REDUCER } from "../../../../redux/BaseHost";
-import { actionTypes } from "./deliveryMonitoringAction";
+import { persistReducer } from 'redux-persist';
+import { PERSIST_REDUCER } from '../../../../redux/BaseHost';
+import { actionTypes } from './deliveryMonitoringAction';
 
 const initialDelivMonitoringState = {
   dataDeverableDoc: null,
-  dataContracts: [],
+  dataJasa: [],
+  dataBarang: [],
 };
 
 export const reducer = persistReducer(
-    PERSIST_REDUCER,
-    (state = initialDelivMonitoringState, action) => {
+  PERSIST_REDUCER,
+  (state = initialDelivMonitoringState, action) => {
     switch (action.type) {
-        case actionTypes.SetDataDeverableDoc: {
-          const { dataDeverableDoc } = action.payload;
-          return { ...state, dataDeverableDoc };
-        }
-        
-        case actionTypes.SetDataContracts: {
-          return {
-            ...state,
-            dataContracts: action.payload,
-          }
-        }
-
-
-        default:
-          return state;
+      case actionTypes.SetDataDeverableDoc: {
+        const { dataDeverableDoc } = action.payload;
+        return { ...state, dataDeverableDoc };
       }
+
+      case actionTypes.SetDataJasa: {
+        return {
+          ...state,
+          dataJasa: action.payload,
+        };
+      }
+
+      case actionTypes.SetDataBarang: {
+        return {
+          ...state,
+          dataBarang: action.payload,
+        };
+      }
+
+      default:
+        return state;
     }
+  }
 );
 
-export const setDataDeverableDoc = dataDeverableDoc => ({ 
-  type: actionTypes.SetDataDeverableDoc, 
-  payload: { dataDeverableDoc } 
+export const setDataDeverableDoc = (dataDeverableDoc) => ({
+  type: actionTypes.SetDataDeverableDoc,
+  payload: { dataDeverableDoc },
 });
