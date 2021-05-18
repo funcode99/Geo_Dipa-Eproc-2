f3aa8ec7c46cbc3d29fcb78f1285837b8ae40979
farhanaziz7 <farhanazizf7@gmail.com>
2021-05-18 17:32:20 +0700

    fix: error not defined

--- src/app/components/modals/index.js
+++ src/app/components/modals/index.js
@@ -3,26 +3,26 @@ import React from 'react';
 import { Container, CustomModal, StyledIcon } from './style';
 
 export const StyledModal = ({
-  visible,
-  onClose,
-  hideCloseIcon = false,
-  disableBackdrop = false,
-  align = '',
-  children,
-  minWidth,
+    visible,
+    onClose,
+    hideCloseIcon = false,
+    disableBackdrop = false,
+    align = '',
+    children,
+    minWidth
 }) => {
-  return (
-    <CustomModal
-      open={visible}
-      onClose={onClose}
-      disableBackdropClick={disableBackdrop}
-    >
-      <Container align={align} minWidth={minWidth}>
-        {hideCloseIcon ? null : (
-          <StyledIcon className="fas fa-times" onClick={onClose} />
-        )}
-        {children}
-      </Container>
-    </CustomModal>
-  );
+    return (
+        <CustomModal
+            open={visible}
+            onClose={onClose}
+            disableBackdropClick={disableBackdrop}
+        >
+            <Container align={align} minWidth={minWidth}>
+                {hideCloseIcon ? null : (
+                    <StyledIcon className="fas fa-times" onClick={onClose} />
+                )}
+                {children}
+            </Container>
+        </CustomModal>
+    );
 };
