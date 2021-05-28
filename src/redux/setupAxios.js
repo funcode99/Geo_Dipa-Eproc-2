2803d3baaecf7ed4ecb13623885608ad2face341
Jeffry Azhari Rosman <jeffry@code.id>
2021-05-28 09:09:22 +0700

    -

--- src/redux/setupAxios.js
+++ src/redux/setupAxios.js
@@ -1,45 +1,53 @@
 import { MODAL } from "../service/modalSession/ModalService";
-import { DEV_NODE } from './BaseHost';
+import { DEV_NODE } from "./BaseHost";
 export default function setupAxios(axios, store) {
   axios.defaults.baseURL = DEV_NODE;
   axios.interceptors.request.use(
-    config => {
+    (config) => {
       const {
-        auth: { authToken }
+        auth: { authToken },
       } = store.getState();
 
       if (authToken) {
         config.headers.Authorization = `Bearer ${authToken}`;
       }
 
-      config.headers.post['Access-Control-Allow-Origin'] = '*';
+      config.headers.post["Access-Control-Allow-Origin"] = "*";
 
       return config;
     },
-    err => Promise.reject(err)
+    (err) => Promise.reject(err)
   );
 
-  axios.interceptors.response.use(function (response) {
-    // console.log("respons:", response );
-    return response;
-  }, function (error) {
-    console.log("error", error);
-    if(error.response?.status === 400 && error.response?.data.message === "TokenExpiredError"){
-      var title = "";
-      var message = "";
-      var button = "";
-      if(localStorage.getItem("i18nConfig") && JSON.parse(localStorage.getItem("i18nConfig")).selectedLang === "id"){
-        title = "Sesi Masuk";
-        message = "Waktu sesi Anda sudah berakhir. Silakan masuk lagi !!";
-        button = "Keluar";
-      }else{
-        title = "Session Log In";
-        message = "Your session time is over. Please sign in again !!";
-        button = "Sign Out";
+  axios.interceptors.response.use(
+    function(response) {
+      // console.log("respons:", response );
+      return response;
+    },
+    function(error) {
+      if (
+        error.response?.status === 400 &&
+        error.response?.data.message === "TokenExpiredError"
+      ) {
+        var title = "";
+        var message = "";
+        var button = "";
+        if (
+          localStorage.getItem("i18nConfig") &&
+          JSON.parse(localStorage.getItem("i18nConfig")).selectedLang === "id"
+        ) {
+          title = "Sesi Masuk";
+          message = "Waktu sesi Anda sudah berakhir. Silakan masuk lagi !!";
+          button = "Keluar";
+        } else {
+          title = "Session Log In";
+          message = "Your session time is over. Please sign in again !!";
+          button = "Sign Out";
+        }
+        MODAL.showSession(title, message, button);
       }
-      MODAL.showSession(title, message, button);
+      console.log("Error interceptors.response => ", error);
+      return Promise.reject(error);
     }
-    console.log("Error interceptors.response => ", error)
-    return Promise.reject(error);
-  });
+  );
 }
