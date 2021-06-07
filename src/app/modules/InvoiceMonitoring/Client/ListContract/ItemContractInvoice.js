7b0c19e477ce6efe802c0ceeba78004de38da3f0
grhgn <gcolzwkwk@gmail.com>
2021-06-07 13:53:08 +0700

    perbaikan warning, tambah halaman faktur pajak

--- src/app/modules/InvoiceMonitoring/Client/ListContract/ItemContractInvoice.js
+++ src/app/modules/InvoiceMonitoring/Client/ListContract/ItemContractInvoice.js
@@ -1,94 +1,89 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "@material-ui/core";
-import { connect, shallowEqual, useSelector } from "react-redux";
-import { FormattedMessage, injectIntl } from "react-intl";
+import React, {
+    useState
+} from 'react';
 import {
-  Card,
-  CardBody,
-  CardFooter,
+    Button,
+} from '@material-ui/core';
+import {
+    connect
+} from "react-redux";
+import {
+    injectIntl
+} from "react-intl";
+import {
+    Card,
+    CardBody
 } from "../../../../../_metronic/_partials/controls";
-import {
-  getContractSummary,
-  saveInvoice,
-  getInvoice,
-} from "../../_redux/InvoiceMonitoringCrud";
-import useToast from "../../../../components/toast";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { rupiah } from "../../../../libs/currency";
-import Navs from "../../../../components/navs";
-import ContractInvoicePage from "./ContractBillingDocument/ContractInvoicePage";
-import ContractSprPage from "./ContractBillingDocument/ContractSprPage";
-import ContractReceiptPage from "./ContractBillingDocument/ContractReceiptPage";
+import Navs from '../../../../components/navs';
+import ContractInvoicePage from './ContractBillingDocument/ContractInvoicePage';
+import ContractSprPage from './ContractBillingDocument/ContractSprPage';
+import ContractReceiptPage from './ContractBillingDocument/ContractReceiptPage';
+import ContractTaxPage from './ContractBillingDocument/ContractTaxPage';
 
 function ItemContractInvoice(props) {
-  const navLists = [
-    {
-      id: "SPR",
-      label: "SPR",
-    },
-    {
-      id: "Invoice",
-      label: "Invoice",
-    },
-    {
-      id: "Kwitansi",
-      label: "Kwitansi",
-    },
-    {
-      id: "Faktur",
-      label: "Faktur Pajak",
-    },
-  ];
 
-  const [navActive, setNavActive] = useState(navLists[0].id);
-  const [Toast, setToast] = useToast();
+    const navLists = [
+        {
+            id: 'SPR',
+            label: 'SPR'
+        },
+        {
+            id: 'Invoice',
+            label: 'Invoice'
+        },
+        {
+            id: 'Kwitansi',
+            label: 'Kwitansi'
+        },
+        {
+            id: 'Faktur',
+            label: 'Faktur Pajak'
+        },
+    ];
 
-  function handleChangeTab(event, newTabActive) {
-    console.log(newTabActive);
-    setNavActive(newTabActive);
-  }
+    const [navActive, setNavActive] = useState(navLists[0].id);
 
-  return (
-    <React.Fragment>
-      <Toast />
-      <Card>
-        <CardBody>
-          <Navs
-            navLists={navLists}
-            handleSelect={(selectedKey) => setNavActive(selectedKey)}
-          />
+    return (
+        <React.Fragment>
+            <Card>
+                <CardBody>
+                    <Navs
+                        navLists={navLists}
+                        handleSelect={(selectedKey) => setNavActive(selectedKey)}
+                    />
 
-          {navActive === "SPR" && (
-            <div className="table-wrapper-scroll-y my-custom-scrollbar my-5 h-100">
-              <ContractSprPage {...props} />
-            </div>
-          )}
+                    {navActive === 'SPR' && (
+                        <div className="table-wrapper-scroll-y my-custom-scrollbar my-5 h-100">
+                            <ContractSprPage {...props} />
+                        </div>
+                    )}
 
-          {navActive === "Invoice" && (
-            <div className="table-wrapper-scroll-y my-custom-scrollbar my-5 h-100">
-              <ContractInvoicePage {...props} />
-            </div>
-          )}
+                    {navActive === 'Invoice' && (
+                        <div className="table-wrapper-scroll-y my-custom-scrollbar my-5 h-100">
+                            <ContractInvoicePage {...props} />
+                        </div>
+                    )}
 
-          {navActive === "Kwitansi" && (
-            <div className="table-wrapper-scroll-y my-custom-scrollbar my-5 h-100">
-              <ContractReceiptPage {...props} />
-            </div>
-          )}
+                    {navActive === 'Kwitansi' && (
+                        <div className="table-wrapper-scroll-y my-custom-scrollbar my-5 h-100">
+                            <ContractReceiptPage {...props} />
+                        </div>
+                    )}
 
-          {navActive === "Faktur" && (
-            <div className="table-wrapper-scroll-y my-custom-scrollbar my-5 h-100"></div>
-          )}
+                    {navActive === 'Faktur' && (
+                        <div className="table-wrapper-scroll-y my-custom-scrollbar my-5 h-100">
+                            <ContractTaxPage {...props} />
+                        </div>
+                    )}
 
-          <div className="d-flex justify-content-end w-100">
-            <Button variant="contained" color="secondary" size="medium">
-              <span className="mr-1">Submit</span>
-            </Button>
-          </div>
-        </CardBody>
-      </Card>
-    </React.Fragment>
-  );
+                    <div className="d-flex justify-content-end w-100">
+                        <Button variant="contained" color="secondary" size="medium">
+                            <span className="mr-1">Submit</span>
+                        </Button>
+                    </div>
+                </CardBody>
+            </Card>
+        </React.Fragment>
+    )
 }
 export default injectIntl(connect(null, null)(ItemContractInvoice));
---
+++ src/app/modules/InvoiceMonitoring/Client/ListContract/service/invoice.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+export function getPicContract(data) {
+    return axios.post(`/invoice/get_pic_contract/${data.id}`, data);
+}
+export function getPicVendor(id) {
+    return axios.get(`/invoice/get_pic_vendor/${id}`);
+}
+export function checkEmail(email) {
+    return axios.get(`/data/check_email?email=${email}`);
+}
+export function updateEmail(data) {
+    return axios.post(`/invoice/update_email/${data.id}`, data);
+}
+export function requestUser(data) {
+    return axios.post(`/invoice/request_users_email/`, data);
+}
+export function deleteUser(data) {
+    return axios.post(`/invoice/delete_pic/`, data);
+}
+export function assignUser(data) {
+    return axios.post(`/invoice/assign_pic/`, data);
+}
+export async function getContractSummary(id) {
+    return axios.get(`/invoice/contract_invoice_sumary/${id}`);
+}
+export async function checkRole(id) {
+    return axios.get(`/invoice/check_role/${id}`);
+}
+export async function getContractClient() {
+    return axios.get(`/invoice/contract_invoice/`);
+}
+export async function getContractVendor(id) {
+    return axios.get(`/invoice/contract_by_vendors/${id}`);
+}
+export async function saveInvoice(data) {
+    return axios.post(`/invoice/contract_invoice_save/`, data);
+}
+export async function getInvoice(contract_id, termin) {
+    return axios.get(`/invoice/get_invoice?contract_id=${contract_id}&term=${termin}`);
+}
