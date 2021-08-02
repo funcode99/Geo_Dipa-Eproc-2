52dfe2b6f831f1b1603285e3b6b7eeea768b9677
Jeffry Azhari Rosman <jeffry@code.id>
2021-08-02 16:06:26 +0700

    update

--- src/app/modules/InvoiceMonitoring/_redux/InvoiceMonitoringCrud.js
+++ src/app/modules/InvoiceMonitoring/_redux/InvoiceMonitoringCrud.js
@@ -257,28 +257,43 @@ export async function createBkb(data) {
 export async function getBkb(id) {
   return axios.get(`/invoice/get_bkb/${id}`);
 }
-export async function tax_manager_approve_bkb(id, tax_man_approved_id, term_id, desc) {
-  return axios.post(`/invoice/tax_manager_approve_bkb`, {
+export async function tax_manager_approve_bkb(
     id,
     tax_man_approved_id,
     term_id,
     desc
+) {
+  return axios.post(`/invoice/tax_manager_approve_bkb`, {
+    id,
+    tax_man_approved_id,
+    term_id,
+    desc,
   });
 }
-export async function finance_manager_approve_bkb(id, finance_man_approved_id, term_id, desc) {
-  return axios.post(`/invoice/finance_manager_approve_bkb`, {
+export async function finance_manager_approve_bkb(
     id,
     finance_man_approved_id,
     term_id,
     desc
+) {
+  return axios.post(`/invoice/finance_manager_approve_bkb`, {
+    id,
+    finance_man_approved_id,
+    term_id,
+    desc,
   });
 }
-export async function finance_director_approve_bkb(id, finance_director_approved_id, term_id, desc) {
-  return axios.post(`/invoice/finance_director_approve_bkb`, {
+export async function finance_director_approve_bkb(
     id,
     finance_director_approved_id,
     term_id,
     desc
+) {
+  return axios.post(`/invoice/finance_director_approve_bkb`, {
+    id,
+    finance_director_approved_id,
+    term_id,
+    desc,
   });
 }
 export async function updateSoftCopyByUser(id, file) {
@@ -346,4 +361,7 @@ export async function getAllMismatch(params) {
 }
 export async function getAllBkb(params) {
   return axios.get(`invoice/get_all_bkb${params ? "?" + params : ""}`);
+}
+export async function getAllInvoice(params) {
+  return axios.get(`invoice/get_invoice_list${params ? "?" + params : ""}`);
 }
