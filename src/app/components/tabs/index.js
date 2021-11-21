cd47fade21feffbd0352178505b78b4e475c9252
Fajar-PC <fajartrigo@gmail.com>
2021-11-21 21:19:24 +0700

    hide form SA for vendor

--- src/app/components/tabs/index.js
+++ src/app/components/tabs/index.js
@@ -66,17 +66,20 @@ export default function Tabs({
       classes={{ root: classes.root, indicator: classes.customIndicatorColor }}
       {...other}
     >
-      {tabLists.map((item) => (
-        <MuiTab
-          key={item.id}
-          label={item.label}
-          icon={item.icon}
-          classes={{
-            root: classes.tabRoot,
-            wrapper: classes.tabWrapper,
-          }}
-        />
-      ))}
+      {tabLists.map(
+        (item) =>
+          item?.display !== "none" && (
+            <MuiTab
+              key={item.id}
+              label={item.label}
+              icon={item.icon}
+              classes={{
+                root: classes.tabRoot,
+                wrapper: classes.tabWrapper,
+              }}
+            />
+          )
+      )}
     </MuiTabs>
   );
 }
