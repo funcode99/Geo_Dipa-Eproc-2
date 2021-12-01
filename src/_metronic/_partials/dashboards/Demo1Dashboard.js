b97a0ee03c2d000ca6a2a402878fa8affa751459
Dwi Susanto <dwisusanto017@gmail.com>
2021-12-01 09:21:09 +0700

    change dashboard user

--- src/_metronic/_partials/dashboards/Demo1Dashboard.js
+++ src/_metronic/_partials/dashboards/Demo1Dashboard.js
@@ -24,30 +24,30 @@ export function Demo1Dashboard() {
           <ListsWidget9 className="card-stretch gutter-b" />
         </div>
 
-        <div className="col-lg-6 col-xxl-4">
-          <StatsWidget11
-            className="card-stretch card-stretch-half gutter-b"
-            symbolShape="circle"
-            baseColor="success"
-          />
-          <StatsWidget12 className="card-stretch card-stretch-half gutter-b" />
-        </div>
+        {/*<div className="col-lg-6 col-xxl-4">*/}
+        {/*  <StatsWidget11*/}
+        {/*    className="card-stretch card-stretch-half gutter-b"*/}
+        {/*    symbolShape="circle"*/}
+        {/*    baseColor="success"*/}
+        {/*  />*/}
+        {/*  <StatsWidget12 className="card-stretch card-stretch-half gutter-b" />*/}
+        {/*</div>*/}
 
-        <div className="col-lg-6 col-xxl-4 order-1 order-xxl-1">
-          <ListsWidget1 className="card-stretch gutter-b" />
-        </div>
-        <div className="col-xxl-8 order-2 order-xxl-1">
-          <AdvanceTablesWidget2 className="card-stretch gutter-b" />
-        </div>
-        <div className="col-lg-6 col-xxl-4 order-1 order-xxl-2">
-          <ListsWidget3 className="card-stretch gutter-b" />
-        </div>
-        <div className="col-lg-6 col-xxl-4 order-1 order-xxl-2">
-          <MixedWidget1 className="card-stretch gutter-b" />
-        </div>
-        <div className="col-lg-12 col-xxl-4 order-1 order-xxl-2">
-          <ListsWidget8 className="card-stretch gutter-b" />
-        </div>
+        {/*<div className="col-lg-6 col-xxl-4 order-1 order-xxl-1">*/}
+        {/*  <ListsWidget1 className="card-stretch gutter-b" />*/}
+        {/*</div>*/}
+        {/*<div className="col-xxl-8 order-2 order-xxl-1">*/}
+        {/*  <AdvanceTablesWidget2 className="card-stretch gutter-b" />*/}
+        {/*</div>*/}
+        {/*<div className="col-lg-6 col-xxl-4 order-1 order-xxl-2">*/}
+        {/*  <ListsWidget3 className="card-stretch gutter-b" />*/}
+        {/*</div>*/}
+        {/*<div className="col-lg-6 col-xxl-4 order-1 order-xxl-2">*/}
+        {/*  <MixedWidget1 className="card-stretch gutter-b" />*/}
+        {/*</div>*/}
+        {/*<div className="col-mlg-12 col-xxl-4 order-1 order-xxl-2">*/}
+        {/*  <ListsWidget8 className="card-stretch gutter-b" />*/}
+        {/*</div>*/}
       </div>
       <div className="row">
         {/* <div className="col-lg-4">
