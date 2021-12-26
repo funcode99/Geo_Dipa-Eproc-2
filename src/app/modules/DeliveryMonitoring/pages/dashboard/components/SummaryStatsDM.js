ca86a233d4aa94b8d5c508589b8b36059137299f
fajar tri p <fajartrigo@gmail.com>
2021-12-26 18:24:27 +0700

    :sparkles: Feature : pie chart on summary contract using contract price

--- src/app/modules/DeliveryMonitoring/pages/dashboard/components/SummaryStatsDM.js
+++ src/app/modules/DeliveryMonitoring/pages/dashboard/components/SummaryStatsDM.js
@@ -1,66 +1,156 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-script-url,jsx-a11y/anchor-is-valid */
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useMemo, useEffect } from "react";
+import objectPath from "object-path";
+import ApexCharts from "apexcharts";
+import { Dropdown } from "react-bootstrap";
+// import {useHtmlClassService} from "../../../layout";
+// import {DropdownMenu4, DropdownCustomToggler} from "../../dropdowns";
+// import {KTUtil} from "../../../_assets/js/components/util";
+import { useHtmlClassService } from "../../../../../../_metronic/layout";
+import {
+  DropdownCustomToggler,
+  DropdownMenu4,
+} from "../../../../../../_metronic/_partials/dropdowns";
+import { KTUtil } from "../../../../../../_metronic/_assets/js/components/util";
 
-const SummaryStatsDM = ({ data, className, authStatus, openModal }) => {
+export default function SumaryStatsDM({ className }) {
+  const uiService = useHtmlClassService();
+
+  const layoutProps = useMemo(() => {
+    return {
+      colorsGrayGray100: objectPath.get(
+        uiService.config,
+        "js.colors.gray.gray100"
+      ),
+      colorsGrayGray700: objectPath.get(
+        uiService.config,
+        "js.colors.gray.gray700"
+      ),
+      colorsThemeBaseSuccess: objectPath.get(
+        uiService.config,
+        "js.colors.theme.base.success"
+      ),
+      colorsThemeLightSuccess: objectPath.get(
+        uiService.config,
+        "js.colors.theme.light.success"
+      ),
+      fontFamily: objectPath.get(uiService.config, "js.fontFamily"),
+    };
+  }, [uiService]);
+
+  useEffect(() => {
+    const element = document.getElementById("kt_mixed_widget_14_chart");
+    if (!element) {
+      return;
+    }
+
+    const height = parseInt(KTUtil.css(element, "height"));
+    const options = getChartOptions(layoutProps, height);
+
+    const chart = new ApexCharts(element, options);
+    chart.render();
+    return function cleanUp() {
+      chart.destroy();
+    };
+  }, [layoutProps]);
+
   return (
-    <div className={`card card-custom bg-gray-100 ${className}`}>
+    <div className={`card card-custom ${className}`}>
       {/* Header */}
-      <div className="card-header border-0 bg-danger py-5">
-        <h3 className="card-title font-weight-bolder text-white">
-          Summary Contract
-        </h3>
+      <div className="card-header border-0 pt-5">
+        <h3 className="card-title font-weight-bolder ">Summary Contract</h3>
       </div>
-      {/* Stat */}
-      <div className="card-spacer mt10">
-        {/* <div className="row m-0"> */}
-        <div className=" bg-light-warning px-8 py-3 rounded-xl mb-7">
-          <span className="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
-            <span className="text-warning font-weight-bolder d-block font-size-lg">
-              {data?.on_progress}
-            </span>
-          </span>
-          <Link
-            // to={`/${authStatus}/delivery-monitoring/contract`}
-            className="text-warning font-weight-bold font-size-h6"
-            onClick={() => openModal("onprogress")}
-          >
-            On Progress
-          </Link>
+      {/* Body */}
+      <div className="card-body d-flex flex-column">
+        <div className="flex-grow-1">
+          <div id="kt_mixed_widget_14_chart" style={{ height: "200px" }}></div>
         </div>
-        <div className=" bg-light-primary px-8 py-3 rounded-xl mb-7">
-          <span className="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
-            <span className="text-primary font-weight-bolder d-block font-size-lg">
-              {data?.sagr}
-            </span>
-          </span>
-          <Link
-            // to={`/${authStatus}/delivery-monitoring/contract`}
-            className="text-primary font-weight-bold font-size-h6"
-            onClick={() => openModal("success")}
+        {/* <div className="pt-5">
+          <p className="text-center font-weight-normal font-size-lg pb-7">
+            Notes: Current sprint requires stakeholders
+            <br />
+            to approve newly amended policies
+          </p>
+          <a
+            href="#"
+            className="btn btn-success btn-shadow-hover font-weight-bolder w-100 py-3"
           >
-            Success
-          </Link>
-        </div>
-        <div className=" bg-light-danger px-8 py-3 rounded-xl mb-7">
-          <span className="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
-            <span className="text-danger font-weight-bolder d-block font-size-lg">
-              {data?.overdue}
-            </span>
-          </span>
-          <Link
-            // to={`/${authStatus}/delivery-monitoring/contract`}
-            className="text-danger font-weight-bold font-size-h6"
-            onClick={() => openModal("overdue")}
-          >
-            Overdue
-          </Link>
-        </div>
-        {/* </div> */}
+            Generate Report
+          </a>
+        </div> */}
       </div>
     </div>
   );
-};
+}
 
-export default SummaryStatsDM;
+function getChartOptions(layoutProps, height) {
+  var options = {
+    chart: {
+      width: "100%",
+      type: "donut",
+    },
+    enabledOnSeries: true,
+    colors: ["#1ab7ea", "#0084ff", "#39539E", "#0077B5"],
+
+    dataLabels: {
+      enabled: false,
+    },
+    series: [76, 67, 61, 90],
+    tooltip: {
+      enabled: true,
+      y: {
+        formatter: function(val) {
+          return val;
+        },
+        title: {
+          formatter: function(seriesName) {
+            return seriesName;
+          },
+        },
+      },
+    },
+    legend: {
+      show: false,
+    },
+  };
+  // const options = {
+  //   series: [74],
+  //   chart: {
+  //     height: height,
+  //     type: "radialBar",
+  //   },
+  //   plotOptions: {
+  //     radialBar: {
+  //       hollow: {
+  //         margin: 0,
+  //         size: "65%",
+  //       },
+  //       dataLabels: {
+  //         showOn: "always",
+  //         name: {
+  //           show: false,
+  //           fontWeight: "700",
+  //         },
+  //         value: {
+  //           color: layoutProps.colorsGrayGray700,
+  //           fontSize: "30px",
+  //           fontWeight: "700",
+  //           offsetY: 12,
+  //           show: true,
+  //         },
+  //       },
+  //       track: {
+  //         background: layoutProps.colorsThemeLightSuccess,
+  //         strokeWidth: "100%",
+  //       },
+  //     },
+  //   },
+  //   colors: [layoutProps.colorsThemeBaseSuccess],
+  //   stroke: {
+  //     lineCap: "round",
+  //   },
+  //   labels: ["Progress"],
+  // };
+  return options;
+}
