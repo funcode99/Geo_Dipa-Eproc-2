cb6f797b0ef90f4c58c479ef70e273cb44a72daf
Fajar Tri <fajartrigo@gmail.com>
2022-07-21 01:02:35 +0700

    :fire: remove unused component

--- src/app/modules/Reports/pages/ReportsPage/components/buttons/FilterDate.js
+++ src/app/modules/Reports/pages/ReportsPage/components/buttons/FilterDate.js
@@ -1,8 +1,7 @@
 import { Menu } from "@material-ui/core";
 import { isEmpty } from "lodash";
 import React, { useCallback, useState } from "react";
-import { Col, Row } from "react-bootstrap";
-import { Dropdown, Form } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
 import { FormattedMessage } from "react-intl";
 
 const FilterDate = ({ setQuery, query }) => {
@@ -101,65 +100,6 @@ const FilterDate = ({ setQuery, query }) => {
           </div>
         </div>
       </Menu>
-
-      {/* <Dropdown className="dropdown-inline" drop="down" alignRight>
-        <Dropdown.Toggle
-          className="btn-primary btn-sm font-weight-bolder dropdown-toggle px-5"
-          variant="transparent"
-          id="dropdown-toggle-top"
-        >
-          Filter by Date
-        </Dropdown.Toggle>
-        <Dropdown.Menu className="dropdown-menu dropdown-menu-xl dropdown-menu-right px-3">
-          <Form.Group as={Row} className={"mb-0"}>
-            <Form.Label column>Start date</Form.Label>
-            <Col sm="8">
-              <input
-                type={"date"}
-                className="form-control form-control-sm"
-                min="0"
-                name={"filter-start-date"}
-                id={"filter-start-date"}
-                placeholder={"Start date ..."}
-                onChange={(e) => setStartDate(e.target.value)}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className={"mb-0"}>
-            <Form.Label column>End date</Form.Label>
-            <Col sm="8">
-              <input
-                type={"date"}
-                className="form-control form-control-sm"
-                min="0"
-                name={"filter-end-date"}
-                id={"filter-end-date"}
-                placeholder={"End date ..."}
-                onChange={(e) => setEndDate(e.target.value)}
-              />
-            </Col>
-          </Form.Group>
-          <div className="d-flex flex-row mt-3 justify-content-end">
-            <button
-              type="button"
-              className="mx-1 float-left btn btn-sm btn-primary"
-              onClick={handleSubmit}
-            >
-              <FormattedMessage id="TITLE.UPDATE" />
-            </button>
-            <button
-              type="button"
-              className="ml-1 float-right btn btn-sm btn-light d-flex"
-              onClick={handleReset}
-            >
-              <i className="fas fa-redo fa-right py-1 mx-1"></i>
-              <span>
-                <FormattedMessage id="TITLE.FILTER.RESET.TABLE" />
-              </span>
-            </button>
-          </div>
-        </Dropdown.Menu>
-      </Dropdown> */}
     </div>
   );
 };
