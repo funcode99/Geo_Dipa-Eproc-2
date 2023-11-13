be62c5a21cb4d108ce82d55febacbdc4b03b5717
Sabila Auliya Chaq <sabilamovie@gmail.com>
2023-11-13 12:19:57 +0700

    fix: bapp authorize official name

--- src/app/modules/DeliveryMonitoring/pages/Termin/BAPP/BappPage.js
+++ src/app/modules/DeliveryMonitoring/pages/Termin/BAPP/BappPage.js
@@ -106,7 +106,6 @@ const BappPage = ({
   const [content, setContent] = React.useState([]);
   const isClient = status === "client";
   const isApproved = taskNews?.approve_status?.code === "approved";
-
   const initialValues = React.useMemo(
     () => ({
       nomor_bapp: taskNews?.no || "",
@@ -119,8 +118,8 @@ const BappPage = ({
       hasil_pekerjaan: taskNews?.review_text || "",
       party1_name : taskNews?.bapp_authorize_officer || "",
       party1_jabatan: taskNews?.bapp_authorize_position || "",
-      party2_name: contract?.contract_party?.party_2_autorize_name,
-      party2_jabatan: contract?.contract_party?.party_2_position,
+      party2_name: taskNews?.bapp_authorize_official_name || contract?.contract_party?.party_2_autorize_name,
+      party2_jabatan: taskNews?.bapp_authorize_official_position || contract?.contract_party?.party_2_position,
     }),
     [taskNews, contract]
   );
@@ -872,6 +871,7 @@ const mapState = (state) => {
     status: auth.user.data.status,
     contract: deliveryMonitoring.dataContractById,
     taskNews: deliveryMonitoring.dataTask?.news,
+    taskNewsBast: deliveryMonitoring.dataTask?.news_bast,
     taskId: deliveryMonitoring.dataTask?.id,
     loadings: {
       upload_s: getLoading(state, keys.upload_s),
